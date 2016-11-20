cb3bb77aadff7bde0a30172d83b1f14900d88d03
Adam Dąbrowski <contact@adamdabrowski.com>
2016-11-20 16:46:24 +0100

    Level 1 finished.

--- public/components.js
+++ public/components.js
@@ -1,6 +1,17 @@
 class StoryBox extends React.Component {
     render() {
-        return (<div>Story Box</div>);
+        const now = new Date();
+        const topicsList = [ 'HTML', 'JavaScript', 'React'];
+
+        return (
+            <div>
+                <h3>Story Box</h3>
+                <p className="lead">Current time: {now.toTimeString()}</p>
+                <ul>
+                    {topicsList.map( topic => <li>{topic}</li>)}
+                </ul>
+            </div>
+        );
     }
 }
 
