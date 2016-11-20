5cd947b83451cdf54bb754ac4ca6240d36c0541d
Adam Dąbrowski <contact@adamdabrowski.com>
2016-11-20 16:57:09 +0100

    Add comments.

--- public/components.js
+++ public/components.js
@@ -10,7 +10,47 @@ class StoryBox extends React.Component {
                 <ul>
                     {topicsList.map( topic => <li>{topic}</li>)}
                 </ul>
+                <CommentBox />
             </div>
+        );
+    }
+}
+
+class Comment extends React.Component {
+
+    render() {
+        return (
+        <div className="comment">
+            <p className="comment-header">{this.props.author}</p>
+            <p className="comment-body">
+                {this.props.body}
+            </p>
+            <div className="comment-footer">
+                <a href="#" className="comment-footer-delete">
+                    Delete comment
+                </a>
+            </div>
+        </div>
+        );
+    }
+}
+
+class CommentBox extends React.Component {
+    
+    
+    render() {
+
+        return (
+        <div className="commentBox">
+            <h3>Comments</h3>
+            <h4 className="comment-count">??? Comments</h4>
+            <div className="comment-list">
+                <Comment 
+                    author="Dude McDudeson" body="Cool stuff, man!" />
+                <Comment 
+                    author="Katherine Jones" body="I'm sexy and I know it" />           
+            </div>
+        </div>
         );
     }
 }
